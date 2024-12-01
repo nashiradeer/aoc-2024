233eccb1a3e6442b08137eae2d226708eb161ee6
NashiraDeer <nashiradeer@gmail.com>
2024-12-01 19:31:02 +0000

    feat: sometimes, these atoms aren't in memory :(

--- lib/aoc2024.ex
+++ lib/aoc2024.ex
@@ -2,8 +2,8 @@ defmodule Aoc2024 do
   def solve(day, part) do
     day_input = File.read!("input/day#{day}.txt")
 
-    module_name = String.to_existing_atom("Elixir.Aoc2024.Day#{day}")
-    function_name = String.to_existing_atom("solve#{part}")
+    module_name = String.to_atom("Elixir.Aoc2024.Day#{day}")
+    function_name = String.to_atom("solve#{part}")
 
     apply(module_name, function_name, [day_input])
   end
